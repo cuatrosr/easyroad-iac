b4aa621024c20e850d915582dd219d7bb74750c8
David Montaño Tamayo <cuatro04sr@gmail.com>
2024-03-04 03:50:42 -0500

    feat: finished iac

---
+++ terraform/modules/helm/release/main.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "kube-prometheus" {
+  name       = "kube-prometheus-stackr"
+  namespace  = "kube-public"
+  version    = "36.2.0"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+}
